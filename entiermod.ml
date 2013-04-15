d4475a5dc1aaa468c21d6a55fe58d8f97f6cb84d
FinMatthieu <matthieu.fin@etu.univ-rouen.fr>
2013-04-15 16:04:14 +0200

    Paramettrage de Entiermod par deux module. Ceci permet d'abstraire l'utilisation de Entier dans Entiermod Et ainsi permettre l'utilisation de Big_int a la place de Gdnb sans modifier Entiermod mais seulement Entier et les module type SigEntier et MakeEntier.

--- entiermod.ml
+++ entiermod.ml
@@ -1,20 +1,35 @@
 #use "gdnb.ml";;
 
-
+(* Type du module contenant la valeur du modulo *)
 module type MakeEntier =
 sig
   type t = Gdnb.gdnb
   val value : t
 end
 
-module Entier =
+(* Type du module qui fourni les fonctions accessible dans Entiermod *)
+module type SigEntier =
+sig
+  module Make (V : sig val value : Gdnb.gdnb end) : MakeEntier
+  type t = Gdnb.gdnb
+  val string_of_entier : Gdnb.gdnb -> string
+  val entier_of_string : string -> Gdnb.gdnb
+  val div : Gdnb.gdnb -> Gdnb.gdnb -> Gdnb.gdnb * Gdnb.gdnb
+  val mul : Gdnb.gdnb -> Gdnb.gdnb -> Gdnb.gdnb
+  val somme : Gdnb.gdnb -> Gdnb.gdnb -> Gdnb.gdnb
+  val inv : Gdnb.gdnb -> Gdnb.gdnb -> Gdnb.gdnb
+end
+  
+(* Entier Fournit des fonctions sur les gdnb ainsi
+   qu'une valeur modulo dans son module Entier.Make *)
+module Entier : SigEntier =
 struct
   type t = Gdnb.gdnb;;
   module Make = functor (V : sig val value : Gdnb.gdnb end) ->
-    struct
-      type t = Gdnb.gdnb;;
-      let value = V.value;;
-    end
+  struct
+    type t = Gdnb.gdnb;;
+    let value = V.value;;
+  end
   let string_of_entier = Gdnb.string_of_gdnb;;
   let entier_of_string = Gdnb.gdnb_of_string;;
   let div = Gdnb.div;;
@@ -23,24 +38,33 @@ struct
   let inv = Gdnb.inv;;
 end
 
-module Entiermod = functor (M : MakeEntier) ->
+(* Entiermod est parametre par deux modules de type
+   SigEntier et MakeEntier. Permettant de creer un module
+   representant Z/M.valueZ *)
+module Entiermod (E : SigEntier) (M : MakeEntier) = 
 struct
-  open Entier
   type t = M.t;;
   let somme (a : string) (b : string) =
-    (Entier.string_of_entier (snd (Entier.div (Entier.somme (Entier.entier_of_string a) (Entier.entier_of_string b)) M.value)))
+    (E.string_of_entier (snd (E.div (E.somme (E.entier_of_string a) (E.entier_of_string b)) M.value)))
   let mul   (a : string) (b : string) =
-    (Entier.string_of_entier (snd (Entier.div (Entier.mul (Entier.entier_of_string a) (Entier.entier_of_string b)) M.value)));;
-  let inv   (a : string)              = (Entier.string_of_entier (Entier.inv (Entier.entier_of_string a) M.value));;
+    (E.string_of_entier (snd (E.div (E.mul (E.entier_of_string a) (E.entier_of_string b)) M.value)));;
+  let inv   (a : string)              = (E.string_of_entier (E.inv (E.entier_of_string a) M.value));;
   let ( $$+ ) = somme;;
   let ( $$* ) = mul;;
 end
 
+
+(* Syntax pour creer des Entiermod : *)
+
+(* N3 = module servant pour le modulo de Entiermod *)
 module N3 = Entier.Make (struct let value = Entier.entier_of_string "3" end);;
-module P3 = Entiermod(N3);;
-module P5 = Entiermod(Entier.Make (struct let value = Entier.entier_of_string "5" end));;
+(* P3 = Z/3Z *)
+module P3 = Entiermod(Entier)(N3);;
+(* P5 = Z/5Z *)
+module P5 = Entiermod(Entier)(Entier.Make (struct let value = Entier.entier_of_string "5" end));;
 
 P3.("3" $$+ "3");;
+
 P5.("3" $$+ "3");;
 
 P5.("3" $$+ "2");;
@@ -53,7 +77,7 @@ P5.inv (P5.("3" $$+ "1324"));;
 
 
 (*
-module Entiermod = functor (M : MakeEntier) ->
+module Entiermod = functor (Gdnb : gdnb) ->
 struct
   module Em = 
   struct
